cb9090d6d874794d573baf377e4eb4da310fb9d9
tehrimzahra <tehrimzahra984@gmail.com>
2024-08-03 10:54:45 +0500

    first commit

---
+++ class4.py
@@ -0,0 +1,6 @@
+
+s = "The quick brown fox jumps over the lazy dog. The fox is clever."
+index_of_fox = s.find("fox")
+print(f"Index of 'fox': {index_of_fox}")
+count_the = s.lower().count("the")
+print(f"Occurrences of 'the': {count_the}")
